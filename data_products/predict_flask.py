ec6d85bedfc575eed3756b2b8376a0fbe1119971
carlotorniai <carlotorniai@gmail.com>
2013-10-26 09:29:11 -0700

    Fixed flask code

--- data_products/predict_flask.py
+++ data_products/predict_flask.py
@@ -22,25 +22,6 @@ print type(trained_model)
 vectorizer = readpickle('vectorizer.pkl')
 print type(vectorizer)
 
-# The methods below returns a json response 
-# p
-@app.route("/predict", methods=['POST'])
-def execute():
-    #pdb.set_trace()
-    url = request.form['url']
-    print url
-    if url!="":
-        # Here predict the label
-        label = predict(trained_model, vectorizer, url)
-        results = "Your article should belong in the " + label + " section of the NYT"
-        print label
-    else:
-        results = "You should post a URL of a NYT article"
-    if request.method == 'POST': 
-        return Response(results, status=200,  mimetype='text/plain')
-    else:
-        return "You shoudl post a URL of a NYT article!!"
-
 @app.route("/parsetext", methods=['POST'])
 def execute_text():
     text = request.form['text']
